5a9a3fa24d957ead2a4f166cfb4f08aef92755f6
Beltraneds <beltranjr.alexander@gmail.com>
2024-11-01 23:19:39 -0300

    Conexion_firebase

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyDLTuAWrWpS_IvQ-hizXOo8Z2xHUM6tpaU",
+  authDomain: "appsad-f3410.firebaseapp.com",
+  projectId: "appsad-f3410",
+  storageBucket: "appsad-f3410.firebasestorage.app",
+  messagingSenderId: "616774467617",
+  appId: "1:616774467617:web:0d57ac8750740c24676360",
+};
+
+const app = initializeApp(config);
